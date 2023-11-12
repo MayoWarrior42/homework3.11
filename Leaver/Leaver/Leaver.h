d61cda1d26880d3f31eb6f23cb9ba0dcae17d9dc
MayoWarrior <sweetyl4255.55@gmail.com>
2023-11-12 13:49:51 +0300

    homework

---
+++ Leaver/Leaver/Leaver.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifdef LEAVER_EXPORTS
+#define LEAVER_API __declspec(dllexport)
+#else
+#define LEAVER_API __declspec(dllimport)
+#endif
+
+#include <string>
+
+class LEAVER_API Leaver
+{
+public:
+    std::string leave(const std::string& name);
+};
---
+++ Leaver/homework3.11.2/homework3.11.2.cpp
@@ -0,0 +1,15 @@
+﻿#include "Leaver.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::cin >> name;
+
+    Leaver leaver;
+    std::string goodbyeMessage = leaver.leave(name);
+    std::cout << goodbyeMessage << std::endl;
+
+    return 0;
+}
