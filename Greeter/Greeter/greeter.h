cc6dd41908c5ecea87d66ee3b864d966b4102c20
MayoWarrior <sweetyl4255.55@gmail.com>
2023-11-12 13:50:19 +0300

    homework1

---
+++ Greeter/Greeter/greeter.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef GREETER_H
+#define GREETER_H
+
+#include <string>
+
+class Greeter {
+public:
+    static std::string greet(const std::string& name);
+};
+
+#endif
---
+++ Greeter/homework3.11.1/homework3.11.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "greeter.h"
+
+int main() {
+    system("chcp 1251");
+    std::string name;
+    std::cout << "Введите ваше имя: ";
+    std::cin >> name;
+
+    std::cout << Greeter::greet(name) << std::endl;
+
+    return 0;
+}
